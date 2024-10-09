42a5060ba98fe05c58eebe47c2a729a1476881e4
unishigure <uni@seanut.app>
2024-10-09 10:16:28 +0900

    refactor: remove unused

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,7 @@
-"use client";
-
-import { useState, useEffect } from "react";
-
 import ContentTable from "@/components/content-table";
 import CreateModal from "@/components/create-modal";
-import { db } from "@/config/db";
-import { Tale } from "@/types";
 
 export default function Home() {
-  // const [tails, setTails] = useState<Tale[]>([]);
-
-  // const fetchTales = async () => {
-  //   const res = await db.tales.toArray();
-  //   res.forEach((value, index) => {
-  //     value.id = (index + 1).toString();
-  //   });
-  //   setTails(res);
-  // };
-
-  // useEffect(() => {
-  //   fetchTales();
-  // }, []);
-
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <ContentTable />
