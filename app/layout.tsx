827b2558f148930181dc3e4d55d4f6b1c0e32ca8
unishigure <uni@seanut.app>
2024-10-11 21:34:49 +0900

    chore: change footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
-import { Link } from "@nextui-org/link";
 import clsx from "clsx";
 
 import { Providers } from "./providers";
@@ -47,16 +46,8 @@ export default function RootLayout({
             <main className="container mx-auto max-w-7xl px-6 flex-grow">
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://nextui.org/"
-                title="nextui.org homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">NextUI</p>
-              </Link>
+            <footer className="w-full flex items-center justify-center py-3 text-gray-600">
+              © SQUARE ENIX
             </footer>
           </div>
         </Providers>
