ba2f9b79e7be93b6512ccfa5cc4cafc87843b41b
unishigure <uni@seanut.app>
2026-01-04 16:47:54 +0900

    fix: correct job category names in localization files and update getJobI18nKey parameter

--- src/app/lib/job.ts
+++ src/app/lib/job.ts
@@ -1,15 +1,25 @@
 import { Job } from "@/app/lib/i18n/messages/ja.json";
 
+export { Job };
+
+export type Role = keyof typeof Job;
+
+export type TankJobCode = keyof typeof Job.Tank;
+export type HealerJobCode = keyof typeof Job.Healer;
+export type MeleeDPSJobCode = keyof typeof Job.Melee;
+export type RangedDPSJobCode = keyof typeof Job.PhysicalRanged;
+export type CasterDPSJobCode = keyof typeof Job.MagicalRanged;
+
 /**
  * Get the i18n key for a given job value.
  *
- * @param value The job value to look up. ex.) "PLD", "WAR"
+ * @param code The job value to look up. ex.) "PLD", "WAR"
  * @returns The i18n key string if found, otherwise null.
  */
-export async function getJobI18nKey(value: string): Promise<string | null> {
-  for (const [categoryKey, category] of Object.entries(Job)) {
-    if (typeof category === "object" && value in category) {
-      return `Job.${categoryKey}.${value}`;
+export async function getJobI18nKey(code: string): Promise<string | null> {
+  for (const [role, jobs] of Object.entries(Job)) {
+    if (typeof jobs === "object" && code in jobs) {
+      return `Job.${role}.${code}`;
     }
   }
   return null;
