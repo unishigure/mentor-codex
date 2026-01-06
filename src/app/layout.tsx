b920294f09e28b09387f48773e5ca1d48042186f
unishigure <uni@seanut.app>
2026-01-06 14:49:00 +0900

    feat: add ImportTale component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { NextIntlClientProvider, useTranslations } from "next-intl";
 
 import { ExportTale } from "@/components/export-tale";
+import { ImportTale } from "@/components/import-tale";
 import { SelectLocale } from "@/components/select-locale";
 
 const geistSans = Geist({
@@ -40,6 +41,7 @@ export default function RootLayout({
             <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4">
               <h1 className="font-bold">{t("title")}</h1>
               <div className="flex items-center gap-4">
+                <ImportTale />
                 <ExportTale />
                 <SelectLocale />
               </div>
